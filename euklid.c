ef1c9951d563ef62de8e524a46b8bf49fdb4b7e7
adriankoehler <mueller4356@t-online.de>
2017-11-20 00:33:10 +0100

    added euklid

--- euklid.c
+++ euklid.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+int euklid(int a, int b){
+  int res = 1;
+  printf("%d\n", res);
+
+  return res;
+}
+
 int main(){
-  
+  int a,b,res;
+  printf("enter two numbers:");
+  scanf("%d %d",&a,&b);
+
+  res = euklid(a,b);
+  printf("%d\n", res);
 
   return 0;
 }
