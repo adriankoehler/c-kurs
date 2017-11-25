f23f38cf88cd460a82384ba7acc7435fefa46d68
adriankoehler <mueller4356@t-online.de>
2017-11-25 12:07:20 +0100

    finished euklid.c

--- euklid.c
+++ euklid.c
@@ -1,19 +1,48 @@
 #include <stdio.h>
 
 int euklid(int a, int b){
-  int res = 1;
-  printf("%d\n", res);
+  //falls a<b -> vertauscht;; A ist größer
+  int tmp;
+  if (a<b){tmp=a;a=b;b=tmp;}
+  tmp=1;
 
-  return res;
+  int res;
+  int temp1a = 1,temp1b = 0;
+  int temp2a = 0,temp2b = 1;
+  int temp1aN = 1,temp1bN = 0;
+
+  printf("    |%3d %3d\n",a,b);
+  puts("---------------");
+  printf("%3d |%3d %3d\n",a,temp1a,temp1b); // 1a 1b
+  printf("%3d |%3d %3d\n",b,temp2a,temp2b); // 2a 2b
+
+  while (b>0){
+    res = a % b;
+    temp1aN = temp1a - (a / b)*temp2a;
+    temp1bN = temp1b - (a / b)*temp2b;
+    // printf("%d-%d*%d=%d / ", temp1a,(a/b),temp2a,temp1aN);
+    // printf("%d-%d*%d=%d\n", temp1b,(a/b),temp2b,temp1bN);
+
+    printf("%3d |%3d %3d\n",res,temp1aN,temp1bN);
+    temp1a = temp2a;
+    temp1b = temp2b;
+    temp2b = temp1bN;
+    temp2a = temp1aN;
+
+    a = b;
+    b = res;
+  }
+
+  return a;
 }
 
 int main(){
   int a,b,res;
-  printf("enter two numbers:");
+  printf("enter two numbers: ");
   scanf("%d %d",&a,&b);
 
   res = euklid(a,b);
-  printf("%d\n", res);
+  printf("\e[1;37m->gcd(%d,%d) = %d\n",a,b,res); //color
 
   return 0;
 }
